9fb290c1081eac8a847a5762d627eade324503ae
Rocky Gray <rocky.grayjr@gmail.com>
2019-10-06 20:41:57 -0400

    basic motion sensor to turn on LED

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,48 @@
 #endif
 
 Ultrasonic ultrasonic(D1);
-uint distance;
+u_int distance;
+u_int numIntervals;
+
+const static unsigned int ONE_SECOND = 1000;
+const static unsigned int SECONDS = ONE_SECOND;
+const static unsigned int ONE_MINUTE = ONE_SECOND * 60;
+const static unsigned int MINUTES = ONE_MINUTE;
+const static unsigned int LIGHT_TIMEOUT = 5 * SECONDS;
+
+const static unsigned int PROXIMITY_THRESHOLD_INCHES = 10;
+bool lightState = false;
 
 void setup() {
     Serial.begin(9600);
+    delay(ONE_SECOND);
     Serial.println("Initializing...");
+    distance = 128;
+    numIntervals = 0;
     pinMode(LED_BUILTIN, OUTPUT);
+    delay(ONE_SECOND * .5);
 }
 
 void loop() {
     distance = ultrasonic.read(INC);
-    Serial.printf("Turning LED off: %d\r\n", distance);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    Serial.printf("distance: %d inches\r\n", distance);
 
-    distance = ultrasonic.read(INC);
-    Serial.printf("Turning LED on: %d\r\n", distance);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    numIntervals = distance < PROXIMITY_THRESHOLD_INCHES
+            ? numIntervals + 1
+            : 0;
+
+    if (numIntervals >= 3) {
+        Serial.println("turning light on");
+        lightState = true;
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(LIGHT_TIMEOUT);
+    }
+
+    if (lightState) {
+        Serial.println("turning light off");
+        lightState = false;
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    delay(100);
 }
